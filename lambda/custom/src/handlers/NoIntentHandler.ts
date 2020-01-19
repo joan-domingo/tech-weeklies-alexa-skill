17832f051bcdf37e591c8c124b9efc644674f1fc
Joan Domingo <joands@gmail.com>
2020-01-19 16:52:46 +0100

    implement no intent handler

--- lambda/custom/src/handlers/NoIntentHandler.ts
+++ lambda/custom/src/handlers/NoIntentHandler.ts
@@ -5,18 +5,29 @@ import {
   RequestHandler
 } from 'ask-sdk-core';
 import { Response } from 'ask-sdk-model';
+import {
+  getSessionAttributes,
+  setSessionAttributes,
+  t
+} from '../util/attributesUtil';
 
 export class NoIntentHandler implements RequestHandler {
   canHandle(input: HandlerInput): Promise<boolean> | boolean {
+    const sessionAttributes = getSessionAttributes(input);
     return (
       getRequestType(input.requestEnvelope) === 'IntentRequest' &&
-      getIntentName(input.requestEnvelope) === 'AMAZON.NoIntent'
+      getIntentName(input.requestEnvelope) === 'AMAZON.NoIntent' &&
+      sessionAttributes.isWaitingForAnAnswer
     );
   }
 
   handle(input: HandlerInput): Promise<Response> | Response {
-    //TODO
-    const speakOutput = 'Good for you';
+    const sessionAttributes = getSessionAttributes(input);
+
+    sessionAttributes.isWaitingForAnAnswer = false;
+    setSessionAttributes(input, sessionAttributes);
+
+    const speakOutput = t(input, 'HELP_MSG');
 
     return input.responseBuilder
       .speak(speakOutput)
